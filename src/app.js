242a5779bbc5966b6c25cc298d0606b8129f33dc
see2 <wse532@126.com>
2023-04-03 23:19:53 +0800

    add task fetch function

--- src/app.js
+++ src/app.js
@@ -8,6 +8,21 @@ app.use(cors());
 
 app.get('/calendar/events', getCalendarEvents);
 
+app.get("/tasks", async (req, res) => {
+  const accessToken = process.env.ACCESS_TOKEN;
+
+  try {
+    const tasksThisWeek = await fetchTasks(accessToken, undefined);
+    const tasksUncompleted = await fetchTasks(accessToken, false);
+
+    res.status(200).json({ tasksThisWeek, tasksUncompleted });
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    res.status(500).json({ error: "Failed to fetch tasks" });
+  }
+});
+
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
