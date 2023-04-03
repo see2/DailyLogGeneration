1cb9747a7a52abe3effdd1f64e98994733eeebb5
see2 <wse532@126.com>
2023-04-03 23:08:15 +0800

    add calender function here and env file

---
+++ src/utils/caldavClient.js
@@ -0,0 +1,14 @@
+const dav = require('dav');
+const url = require('url');
+
+const { CALDAV_URL, CALDAV_EMAIL, CALDAV_PASSWORD } = process.env;
+
+const caldavClient = new dav.Client(
+  new dav.Transport(new dav.Credentials({ username: CALDAV_EMAIL, password: CALDAV_PASSWORD })),
+  {
+    baseUrl: url.resolve(CALDAV_URL, '/remote.php/dav'),
+    sandbox: new dav.Sandbox(),
+  }
+);
+
+module.exports = caldavClient;
