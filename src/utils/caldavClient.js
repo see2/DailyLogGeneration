b569b2994c55fd6e4412e5844b9d575788504fbe
see2 <wse532@126.com>
2023-04-04 00:20:04 +0800

    the transport function revised

--- src/utils/caldavClient.js
+++ src/utils/caldavClient.js
@@ -4,7 +4,7 @@ const url = require('url');
 const { CALDAV_URL, CALDAV_EMAIL, CALDAV_PASSWORD } = process.env;
 
 const caldavClient = new dav.Client(
-  new dav.Transport(new dav.Credentials({ username: CALDAV_EMAIL, password: CALDAV_PASSWORD })),
+  new dav.transport.OAuth2(new dav.Credentials({ username: CALDAV_EMAIL, password: CALDAV_PASSWORD })),
   {
     baseUrl: url.resolve(CALDAV_URL, '/remote.php/dav'),
     sandbox: new dav.Sandbox(),
